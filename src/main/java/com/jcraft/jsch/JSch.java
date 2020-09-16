ae6dbb08f350c021b408d088bee4d439e234ddde
Andreas Rosdal <41650711+andreasrosdal@users.noreply.github.com>
2020-09-16 18:59:30 +0200

    Update version

--- src/main/java/com/jcraft/jsch/JSch.java
+++ src/main/java/com/jcraft/jsch/JSch.java
@@ -36,7 +36,7 @@ public class JSch{
   /**
    * The version number.
    */
-  public static final String VERSION  = "0.1.54";
+  public static final String VERSION  = "0.2.0";
 
   static java.util.Hashtable config=new java.util.Hashtable();
   static{
